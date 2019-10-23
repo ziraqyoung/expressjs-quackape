072f68233cd8e323d59429b0384f657f2c0a25e1
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-23 12:10:12 +0300

    Create User model

---
+++ models/User.js
@@ -0,0 +1,72 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true },
+    password: String,
+    passwordResetToken: String,
+    passwordResetExpires: String,
+    github: String,
+    profile: {
+      name: String,
+      gender: String,
+      location: String,
+      website: String,
+      picture: String
+    }
+  },
+  { timestamps: true }
+);
+/**
+ * Hooks
+ */
+userSchema.pre("save", function save() {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+/**
+ * Helper method for validating user's password
+ */
+userSchema.methods.comparePassword = function comparePassword(
+  candidatePassword,
+  cb
+) {
+  bcrypt.compare(candidatePassword, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+/**
+ * Helper for getting user's avatar
+ */
+userSchema.methods.gravatar = function gravatar(size = 200) {
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?size=${size}&d=retro`;
+  }
+  const md5 = crypto
+    .createHash("md5")
+    .update(this.email)
+    .digest("hex");
+  return `https://gravatar.com/avatar/${md5}?size=${size}&d=retro`;
+};
+/**
+ * Model
+ */
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
