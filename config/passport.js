5a0be4b3462a6284513bd168bb14dd9b234fb209
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-24 16:33:01 +0300

    Configure app to use passport local startegy with email/password

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const { LocalStrategy: Strategy } = require("passport-local");
+const User = require("../models/User");
+/**
+ * Passport configurations
+ */
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+/**
+ * Sign in with email and password
+ */
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    User.findOne({ email: email.toLowercase() }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { msg: `Email ${email} not found` });
+      }
+      user.comparePassword(password, (err, isMatch) => {
+        if (err) {
+          return done(err);
+        }
+        if (isMatch) {
+          return done(null, user);
+        }
+        return done(null, false, { msg: "Invalid email password combination" });
+      });
+    });
+  })
+);
