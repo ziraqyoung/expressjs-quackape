2d82bf86d80afe36eb717e9a88e5025c02e956a3
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-24 16:22:19 +0300

    Configure app to use passport

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const sass = require("node-sass-middleware");
 const errorHandler = require("errorhandler");
 const chalk = require("chalk");
 const mongoose = require("mongoose");
+const passport = require("passport");
 /**
  * Load environment variable from .env for configurations and API Keys
  */
@@ -43,6 +44,8 @@ app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(logger("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
