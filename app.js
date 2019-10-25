c062b862f41cdf44388d2b0ed721802852c57ada
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-26 01:09:16 +0300

    Reorganize session middleware configuration in app.js

--- app.js
+++ app.js
@@ -52,11 +52,6 @@ app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(logger("dev"));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
 app.use(
   session({
     resave: true,
@@ -69,6 +64,11 @@ app.use(
     })
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
 app.use(
   sass({
     src: path.join(__dirname, "public"),
