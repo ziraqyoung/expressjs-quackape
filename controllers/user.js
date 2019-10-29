dff46ada84acc41515bae2dd12c05150753df5b0
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-30 01:06:12 +0300

    Implement POST /signup logic to create a local account

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const validator = require("validator");
 const passport = require("passport");
+const User = require("../models/User");
 /**
  * GET /login (login page)
  */
@@ -50,4 +51,45 @@ exports.getSignup = (req, res) => {
     return res.redirect("/");
   }
   res.render("account/signup", { title: "Create Account" });
+};
+/**
+ * POST /signup
+ * Create a new local account
+ */
+exports.postSignup = (req, res, next) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email" });
+  if (!validator.isLength(req.body.password, { min: 8 }))
+    validationErrors.push({
+      msg: "Password must be atleast 8 characters long"
+    });
+  if (req.body.password !== req.body.confirmPassword)
+    validationErrors.push({ msg: "Password do not match" });
+
+  if (validationErrors.lenght) {
+    req.flash("errors", validationErrors);
+    return res.redirect("/signup");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false
+  });
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  User.findOne({ email: req.body.email }, (err, existingUser) => {
+    if (err) return next(err);
+    if (existingUser) {
+      req.flash("errors", { msg: "Account with that email already exists" });
+      return res.redirect("/signup");
+    }
+    user.save(err => {
+      if (err) return next(err);
+      req.logIn(user, err => {
+        if (err) return next(err);
+      });
+      res.redirect("/");
+    });
+  });
 };
